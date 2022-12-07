5fbfffb51829a82485b3d29de5408a4582a22385
Grigory <grishka5556@gmail.com>
2022-12-07 16:10:41 +0300

    cursor pointer

--- js/script.js
+++ js/script.js
@@ -1,55 +1,70 @@
-const allNavButtons = document.querySelectorAll('.menu-main-block__list');
+const allNavButtons = document.querySelectorAll(".menu-main-block__list")
 allNavButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
+    button.addEventListener("click", (event) => {
         allNavButtons.forEach((button) => {
-            button.classList.remove('menu-main-block__list_active');
-        });
-        const { target } = event;
-        target.classList.add('menu-main-block__list_active');
-    });
-});
+            button.classList.remove("menu-main-block__list_active")
+        })
+        const { target } = event
+        target.classList.add("menu-main-block__list_active")
+    })
+})
 
 //button checkbox
-const checkButton = document.querySelector('.checkbox__real')
+const checkButton = document.querySelector(".checkbox__real")
 
-if(checkButton) {
-    checkButton.addEventListener('click', () => {
-    checkButton.classList.toggle('checkbox__reals');   
-    }) 
-    
-} 
+if (checkButton) {
+    checkButton.addEventListener("click", () => {
+        checkButton.classList.toggle("checkbox__reals")
+    })
+}
 //button radio
-const checkRadio = document.querySelector('.radio__checkbox')
+// const checkRadio = document.querySelector(".radio__checkbox")
+// const checkRadioTwo = document.querySelector(".radio__checkbox_2")
 
-if(checkRadio) {
-    checkRadio.addEventListener('click', () => {
-        checkRadio.classList.toggle('radio__checkboxx');   
-    }) 
-} 
-const checkRadioTwo = document.querySelector('.radio__checkbox_2')
+// if (checkRadio) {
+//     checkRadio.addEventListener("click", () => {
+//         checkRadio.classList.toggle("radio__checkboxx")
+//     })
+// }
 
-if(checkRadioTwo) {
-    checkRadioTwo.addEventListener('click', () => {
-        checkRadioTwo.classList.toggle('radio__checkboxx');   
-    }) 
-} 
+// if (checkRadioTwo) {
+//     checkRadioTwo.addEventListener("click", () => {
+//         checkRadioTwo.classList.toggle("radio__checkboxx")
+//     })
+// }
 
+const checkRadio = document.querySelector(".radio__checkbox")
+const checkRadioTwo = document.querySelector(".radio__checkbox_2")
+let switchRadio = function () {
+    if (checkRadio) {
+        checkRadio.addEventListener("click", () => {
+            checkRadio.classList.toggle("radio__checkboxx")
+        })
+    }
+
+    if (checkRadioTwo) {
+        checkRadioTwo.addEventListener("click", () => {
+            checkRadioTwo.classList.toggle("radio__checkboxx")
+        })
+    }
+}
+
+switchRadio()
 //Проверка заполнения телефона
-let inp = document.querySelector('#tel');
+let inp = document.querySelector("#tel")
 
 // Проверяем фокус
-inp.addEventListener('focus', _ => {
-  // Если там ничего нет или есть, но левое
-  if(!/^\+\d*$/.test(inp.value))
-    // То вставляем знак плюса как значение
-    inp.value = '+';
-});
+inp.addEventListener("focus", (_) => {
+    // Если там ничего нет или есть, но левое
+    if (!/^\+\d*$/.test(inp.value))
+        // То вставляем знак плюса как значение
+        inp.value = "+"
+})
 
-inp.addEventListener('keypress', e => {
-  // Отменяем ввод не цифр
-  if(!/\d/.test(e.key))
-    e.preventDefault();
-});
+inp.addEventListener("keypress", (e) => {
+    // Отменяем ввод не цифр
+    if (!/\d/.test(e.key)) e.preventDefault()
+})
 // const hoverBlock = document.querySelector('.photo-main-block__items');
 // const hoverNewBlock = document.querySelector('.photo-main-block__footer');
 // hoverBlock.addEventListener('mouseover', () => {
@@ -61,19 +76,14 @@ inp.addEventListener('keypress', e => {
 //         // console.log('privet');
 // });
 
-
 // let p = document.querySelector('.photo-main-block__img');
 // let c = document.querySelectorAll('.photo-main-block__footer');
 
 // p.addEventListener('mouseover', e => c.forEach(a => a.classList.add("photo-main-block__footer_hover")));
 // p.addEventListener('mouseout', e => c.forEach(a => a.classList.remove("photo-main-block__footer_hover")));
 
-
 // const hoverBlock = document.getElementsByClassName('photo-main-block__items');
 // const hoverNewBlock = document.getElementsByClassName('photo-main-block__footer');
 
 // hoverBlock.onmouseover = function(){hoverNewBlock.classList.add('hover')};
 // hoverBlock.onmouseout = function(){hoverNewBlock.classList.remove('hover')};
-
-
-
