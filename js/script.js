2cbf03417e9cc7ca8660ab7d67a80d7c3ed22a40
Grigory <grishka5556@gmail.com>
2022-12-17 12:56:32 +0300

    radio button text

--- js/script.js
+++ js/script.js
@@ -9,29 +9,6 @@ allNavButtons.forEach((button) => {
     })
 })
 
-//button checkbox
-// const checkButton = document.querySelector(".checkbox__real")
-
-// if (checkButton) {
-//     checkButton.addEventListener("click", () => {
-//         checkButton.classList.toggle("checkbox__reals")
-//     })
-// }
-// Переключатель radio
-// const spans = document.querySelectorAll(".radio__checkbox")
-// spans.forEach((span) => {
-//     span.addEventListener("click", () => {
-//         spans.forEach((sp) => {
-//             if (sp !== span) {
-//                 sp.classList.remove("radio__checkboxx")
-//                 sp.previousElementSibling.setAttribute("checked", "false")
-//             }
-//         })
-//         span.classList.toggle("radio__checkboxx")
-//         span.previousElementSibling.setAttribute("checked", "true")
-//     })
-// })
-
 //Проверка заполнения телефона
 let inp = document.querySelector("#tel")
 
@@ -47,25 +24,3 @@ inp.addEventListener("keypress", (e) => {
     // Отменяем ввод не цифр
     if (!/\d/.test(e.key)) e.preventDefault()
 })
-// const hoverBlock = document.querySelector('.photo-main-block__items');
-// const hoverNewBlock = document.querySelector('.photo-main-block__footer');
-// hoverBlock.addEventListener('mouseover', () => {
-//     hoverNewBlock.classList.add('hover');
-//     //    console.log('privet');
-//     });
-// hoverBlock.addEventListener('mouseout', () => {
-//     hoverNewBlock.classList.remove('hover');
-//         // console.log('privet');
-// });
-
-// let p = document.querySelector('.photo-main-block__img');
-// let c = document.querySelectorAll('.photo-main-block__footer');
-
-// p.addEventListener('mouseover', e => c.forEach(a => a.classList.add("photo-main-block__footer_hover")));
-// p.addEventListener('mouseout', e => c.forEach(a => a.classList.remove("photo-main-block__footer_hover")));
-
-// const hoverBlock = document.getElementsByClassName('photo-main-block__items');
-// const hoverNewBlock = document.getElementsByClassName('photo-main-block__footer');
-
-// hoverBlock.onmouseover = function(){hoverNewBlock.classList.add('hover')};
-// hoverBlock.onmouseout = function(){hoverNewBlock.classList.remove('hover')};
