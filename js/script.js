16ad3e23df51bff2857b0c6b5321f53d68386000
Grigory <grishka5556@gmail.com>
2022-12-22 08:21:26 +0300

    page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-// const allNavButtons = document.querySelectorAll(".choice__list")
-// allNavButtons.forEach((button) => {
-//     button.addEventListener("click", (event) => {
-//         allNavButtons.forEach((button) => {
-//             button.classList.remove("choice__list_active")
-//         })
-//         const { target } = event
-//         target.classList.add("choice__list_active")
-//     })
-// })
+const allNavButtons = document.querySelectorAll(".choice__list")
+allNavButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        allNavButtons.forEach((button) => {
+            button.classList.remove("choice__list_active")
+        })
+        const { target } = event
+        target.classList.add("choice__list_active")
+    })
+})
 
 
-// let inp = document.querySelector("#tel")
+let inp = document.querySelector("#tel")
 
 
-// inp.addEventListener("focus", (_) => {
-//     if (!/^\+\d*$/.test(inp.value))
-//         inp.value = "+"
-// })
+inp.addEventListener("focus", (_) => {
+    if (!/^\+\d*$/.test(inp.value))
+        inp.value = "+"
+})
 
-// inp.addEventListener("keypress", (e) => {
-//     if (!/\d/.test(e.key)) e.preventDefault()
-// })
+inp.addEventListener("keypress", (e) => {
+    if (!/\d/.test(e.key)) e.preventDefault()
+})
