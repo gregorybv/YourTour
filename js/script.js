3ba074ff6d02d04ec5b046f9bdcf5819b9d41aa0
Grigory <grishka5556@gmail.com>
2022-12-22 07:57:46 +0300

    not js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
-const allNavButtons = document.querySelectorAll(".choice__list")
-allNavButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        allNavButtons.forEach((button) => {
-            button.classList.remove("choice__list_active")
-        })
-        const { target } = event
-        target.classList.add("choice__list_active")
-    })
-})
+// const allNavButtons = document.querySelectorAll(".choice__list")
+// allNavButtons.forEach((button) => {
+//     button.addEventListener("click", (event) => {
+//         allNavButtons.forEach((button) => {
+//             button.classList.remove("choice__list_active")
+//         })
+//         const { target } = event
+//         target.classList.add("choice__list_active")
+//     })
+// })
 
-//Проверка заполнения телефона
-let inp = document.querySelector("#tel")
 
-// Проверяем фокус
-inp.addEventListener("focus", (_) => {
-    // Если там ничего нет или есть, но левое
-    if (!/^\+\d*$/.test(inp.value))
-        // То вставляем знак плюса как значение
-        inp.value = "+"
-})
+// let inp = document.querySelector("#tel")
 
-inp.addEventListener("keypress", (e) => {
-    // Отменяем ввод не цифр
-    if (!/\d/.test(e.key)) e.preventDefault()
-})
+
+// inp.addEventListener("focus", (_) => {
+//     if (!/^\+\d*$/.test(inp.value))
+//         inp.value = "+"
+// })
+
+// inp.addEventListener("keypress", (e) => {
+//     if (!/\d/.test(e.key)) e.preventDefault()
+// })
