46c2d0b5bc6f22131c0485669d1687689971a87d
Grigory <grishka5556@gmail.com>
2023-01-08 22:26:02 +0300

    style webstorm

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const allNavButtons = document.querySelectorAll(".choice__link");
 allNavButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        allNavButtons.forEach((button) => {
-            button.classList.remove("choice__link_active");
-        });
-        const { target } = event;
-        target.classList.add("choice__link_active");
+  button.addEventListener("click", (event) => {
+    allNavButtons.forEach((button) => {
+      button.classList.remove("choice__link_active");
     });
+    const {target} = event;
+    target.classList.add("choice__link_active");
+  });
 });
 
 let inp = document.querySelector("#tel");
 
 inp.addEventListener("focus", (_) => {
-    if (!/^\+\d*$/.test(inp.value)) inp.value = "+";
+  if (!/^\+\d*$/.test(inp.value)) inp.value = "+";
 });
 
 inp.addEventListener("keypress", (e) => {
-    if (!/\d/.test(e.key)) e.preventDefault();
+  if (!/\d/.test(e.key)) e.preventDefault();
 });
