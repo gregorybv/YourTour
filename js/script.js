24adc1369cc7471364511b82adff1e81e7c7ee84
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-10 20:54:51 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -22,9 +22,81 @@ inp.addEventListener("keypress", (e) => {
 
 // Scroll
 
-document.body.onscroll = function() {
-  document.querySelector('.header__list').classList.add('header__link_scroll')
-  document.querySelector('.header__link_phone').classList.add('header__link_scroll')
-  document.querySelector('.header__logo').classList.add('header__link_scroll')
-  document.querySelector('.header__hat').classList.add('header__hat_scroll')
-}
+// document.body.onscroll = function() {
+//   if ('.header__blocks' ==)
+//
+//
+//   document.querySelector('.header__list').classList.add('header__link_scroll')
+//   document.querySelector('.header__link_phone').classList.add('header__link_scroll')
+//   document.querySelector('.header__logo').classList.add('header__link_scroll')
+//   document.querySelector('.header__blocks').classList.add('header__blocks_scroll')
+// }
+
+// const content = document.querySelector('.header__img')
+// const blocks = document.querySelector('.header__blocks')
+// const link = document.querySelector('.header__list')
+// const phone = document.querySelector('.header__link_phone')
+// const logo = document.querySelector('.header__logo')
+
+// if (blocks == content) {
+//   blocks.classList.remove('header__blocks_scroll')
+//   link.classList.remove('header__link_scroll')
+//   phone.classList.remove('header__link_scroll')
+//   logo.classList.remove('header__link_scroll')
+// } else {
+//   blocks.classList.add('header__blocks_scroll')
+//   link.classList.add('header__link_scroll')
+//   phone.classList.add('header__link_scroll')
+//   logo.classList.add('header__link_scroll')
+// }
+
+
+// window.onscroll = function () { // отслеживаем скролл
+//   const content = document.querySelector('.header__img')
+// const blocks = document.querySelector('.header__blocks')
+// const link = document.querySelector('.header__list')
+// const phone = document.querySelector('.header__link_phone')
+// const logo = document.querySelector('.header__logo')
+//   const z = document.body.scrollTop; // Получаем высоту передвигаемого сролла
+  // const pixels = 600; // Указываем количество пикселей
+  // if (z > pixels){
+//     blocks.classList.add('header__blocks_scroll')
+//     link.classList.add('header__link_scroll')
+//     phone.classList.add('header__link_scroll')
+//     logo.classList.add('header__link_scroll')  }
+//   console.log('что-то')
+// }
+// if (document.body.scrollTop > 100) {
+//   document.querySelector('.header__blocks').classList.add('header__blocks_scroll')
+//   blocks.classList.add('header__blocks_scroll');
+//   phone.classList.add('header__link_scroll')
+// }
+
+const blocks = document.querySelector('.header__blocks');
+// Элемент иконки
+const text = document.querySelector('.header__blocks');
+// Блок или секция
+
+const link = document.querySelector('.header__list')
+const phone = document.querySelector('.header__link_phone')
+const logo = document.querySelector('.header__logo')
+
+
+document.addEventListener('scroll', () => {
+  let stepColor = text.getBoundingClientRect().height;
+  let scrollY = window.scrollY;
+
+  if (scrollY > stepColor) {
+    blocks.classList.add('header__blocks_scroll')
+    link.classList.add('header__link_scroll')
+    phone.classList.add('header__link_scroll')
+    logo.classList.add('header__logo_scroll')
+  } else {
+    blocks.classList.remove('header__blocks_scroll')
+    link.classList.remove('header__link_scroll')
+    phone.classList.remove('header__link_scroll')
+    logo.classList.remove('header__logo_scroll')
+  }
+
+
+})
