3e5b4af004c755ecfc06ec5e26b74bf8fc1e260c
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 09:03:18 +0300

    new edits

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ allNavButtons.forEach((button) => {
     allNavButtons.forEach((button) => {
       button.classList.remove("choice__link_active");
     });
-    const {target} = event;
+    const { target } = event;
     target.classList.add("choice__link_active");
   });
 });
@@ -19,28 +19,26 @@ inp.addEventListener("keypress", (e) => {
   if (!/\d/.test(e.key)) e.preventDefault();
 });
 
-
 // Scroll
+const blocks = document.querySelector(".header__blocks");
+const text = document.querySelector(".header__blocks");
+const link = document.querySelector(".header__list");
+const phone = document.querySelector(".header__link_phone");
+const logo = document.querySelector(".header__logo");
 
-const blocks = document.querySelector('.header__blocks');
-const text = document.querySelector('.header__blocks');
-const link = document.querySelector('.header__list')
-const phone = document.querySelector('.header__link_phone')
-const logo = document.querySelector('.header__logo')
-
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   let stepColor = text.getBoundingClientRect().height;
   let scrollY = window.scrollY;
 
   if (scrollY > stepColor) {
-    blocks.classList.add('header__blocks_scroll')
-    link.classList.add('header__link_scroll')
-    phone.classList.add('header__link_scroll')
-    logo.classList.add('header__logo_scroll')
+    blocks.classList.add("header__blocks_scroll");
+    link.classList.add("header__link_scroll");
+    phone.classList.add("header__link_scroll");
+    logo.classList.add("header__logo_scroll");
   } else {
-    blocks.classList.remove('header__blocks_scroll')
-    link.classList.remove('header__link_scroll')
-    phone.classList.remove('header__link_scroll')
-    logo.classList.remove('header__logo_scroll')
+    blocks.classList.remove("header__blocks_scroll");
+    link.classList.remove("header__link_scroll");
+    phone.classList.remove("header__link_scroll");
+    logo.classList.remove("header__logo_scroll");
   }
-})
+});
